b0dab6de1a4e032555cba5dec05b086f49a57a1d
Yousef <yousefjooo2018@gmail.com>
2023-04-29 15:28:16 +0300

    add check on root user in task2.sh

--- lab1/task2.sh
+++ lab1/task2.sh
@@ -2,12 +2,17 @@
 #########to be executed every 1 min to monitor system load, and add it to file /var/log/system-load. The script must be run using root.
 ##Exit codes:
 ##	0 : Normal terminated
+##	1: the user is not the root
+
+# check if the user who run the script is the root
+[ ${EUID} -ne 0 ] && exit 1
 
 while true
 do
+
     # Get the system load average for the last minute
     load=$(uptime)
-
+	
     # Get the current date and time
     timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
